572e3c948179f856dda29cede20c37d3404e2ee7
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-09-09 14:50:46 -0500

    9/9

--- prog_93a.gd
+++ prog_93a.gd
@@ -5,9 +5,15 @@ extends Control
 func _on_btn_calc_pressed() -> void:
 	var killowatts = int($txtKillowatts.text)
 	var base = int(killowatts * 0.0475)
-	var sur = float(base * 1.1)
-	var tax = float(base * 1.03)
-	# here
+	var sur = float(base * 0.1)
+	var tax = float(base * 0.03)
+	var total = float(base + sur + tax)
+	var late = float(total * 1.04)
+	$lblBase.text = "$ %.2f" % base
+	$lblSurcharge.text = "$ %.2f" % sur
+	$lblCitytax.text = "$ %.2f" % tax
+	$lblTotalPay.text = "$ %.2f" % total
+	$lblLatePay.text = "$ %.2f" % late
 
 
 func _on_btn_clear_pressed() -> void:
