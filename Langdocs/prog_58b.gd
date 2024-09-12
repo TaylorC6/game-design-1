584e228eb5bab9518393d6dbfe776ff3861d1b30
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-09-12 13:51:15 -0500

    9-12

--- Langdocs/prog_58b.gd
+++ Langdocs/prog_58b.gd
@@ -6,8 +6,8 @@ func _on_btn_calc_pressed() -> void:
 	var a = float($txta.text)
 	var b = float($txtb.text)
 	var c = float($txtc.text)
-	var quadadd = (-b + sqrt(b ** 2 - 4 * a * c)) / 2
-	var quadsub = (-b - sqrt(b ** 2 - 4 * a * c)) / 2
+	var quadadd = (-b + sqrt(b ** 2 - 4 * a * c)) / 2 * a
+	var quadsub = (-b - sqrt(b ** 2 - 4 * a * c)) / 2 * a
 	$lblRoots.text = "Roots: " + str(quadadd) + \
 					"\t , " + str(quadsub)
 
