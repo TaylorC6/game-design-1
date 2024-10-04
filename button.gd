131c2ad0163650d298527093cd850191af1a1a6f
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-10-04 12:44:56 -0500

    10-4

---
+++ button.gd
@@ -0,0 +1,17 @@
+extends Button
+
+var dragging = false
+var offset = Vector2.ZERO
+
+func _process(delta):
+	if dragging:
+		self.global_position = get_global_mouse_position() - offset
+
+
+func _on_button_down() -> void:
+	dragging = true
+	offset = get_global_mouse_position() - self.global_position
+
+
+func _on_button_up() -> void:
+	dragging = false
