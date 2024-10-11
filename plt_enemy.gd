5b7a730b85170d4ae739ba2d8a1eec7e828c49de
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-10-11 14:35:20 -0500

    10-11

--- plt_enemy.gd
+++ plt_enemy.gd
@@ -3,7 +3,6 @@ extends RigidBody2D
 var speed = 100
 var direction = -1
 
-
 func _ready() -> void:
 	$Timer.start()
 
@@ -19,9 +18,9 @@ func _on_body_entered(body: Node) -> void:
 		OS.alert("You Died!")
 		get_tree().reload_current_scene()
 
-	update_animation(direction)
+	update_animation()
 
 @onready var anim = $AnimatedSprite2D
-func update_animation(direction):
+func update_animation():
 	anim.play("default")
 	anim.flip_h = direction < 0 # flip sprite if moving left
