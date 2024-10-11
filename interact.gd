eed1b6c02353c80100b689e1d7c9d82f4a014c21
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-10-11 15:20:42 -0500

    10-11

--- interact.gd
+++ interact.gd
@@ -1,7 +1,12 @@
 extends RigidBody2D
 
 
-func _on_mouse_entered() -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		self.queue_free() # Delete self from world
- 
+func _on_body_entered(body: Node) -> void:
+	$Timer.start()
+	if body.name == "plt_player":
+		for num in range(101):
+			self.position.y += 1
+
+func _on_timer_timeout():
+		for num in range(101):
+			self.position.y -= 1
