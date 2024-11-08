82ecbfd9f12f7beab910406a56af01c80a6c038c
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2024-11-08 14:30:11 -0600

    11/8

--- td_player.gd
+++ td_player.gd
@@ -73,6 +73,24 @@ func pickup_health(value):
 func pickup_money(value):
 	data.money += value
 
+signal health_depleted
+
+func take_damage(dmg):
+	if damage_lock == 0:
+		data.health -= dmg
+		data.state = STATES.DAMAGED
+		damage_lock = 0.5
+		animation_lock = dmg * 0.005
+		# TODO: damage shader
+		if data.health > 0:
+			# TODO: Play damage sound
+			pass
+		else:
+			data.state = STATES.DEAD
+			# TODO: play death anim and sound
+		await get_tree().create_timer(0.5).timeout
+		health_depleted.emit()
+	pass
 
 func _physics_process(delta: float) -> void:
 	animation_lock = max(animation_lock-delta, 0.0)
